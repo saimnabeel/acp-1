c54999168c27ccbcf346e9cfd565a46eeafb8658
saimnabeel <Saimnabeeliqbal@gmail.com>
2026-07-20 15:19:12 +0500

    Add files via upload

---
+++ acp1.py
@@ -0,0 +1,25 @@
+#greeting message
+
+
+import colorama 
+colorama.init()
+print(colorama.Fore.RED
+       + "Hello, welcome to the program!")
+
+#print my name
+print("My name is Saimnabeel.\n")
+ #print my hobby
+print("My hobby is playing minecraft.\n")
+#print my favorite color
+print("My favorite color is blue and purple.\n")
+#math eqaution
+print("The result of 5 + 3 is:", 5 + 3)
+
+#importing colors in text 
+colorama.init()
+
+
+
+import colorama
+colorama.init()
+print(colorama.Fore.GREEN + "Hello, welcome to the program!")
